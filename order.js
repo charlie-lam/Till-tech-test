56014f39a0bc8a455073545807876f5a668e6e36
charlie-lam <93406456+charlie-lam@users.noreply.github.com>
2023-05-17 21:59:35 +0100

    addItem slight refactor

--- order.js
+++ order.js
@@ -12,10 +12,10 @@ class Order {
   }
 
   addItem(item) {
-    const found = data.prices[0][item]
-    if(found){
-      this.items[item] ? this.items[item] += 1 : this.items[item] = 1
-      this.total += found
+    const foundPrice = data.prices[0][item]
+    if(foundPrice){
+      this.items[item] ? this.items[item] ++ : this.items[item] = 1
+      this.total += foundPrice
     }
     else{
       throw new Error('Item not in list')
